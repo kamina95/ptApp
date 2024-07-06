b31483cc3c0f75eb4fa744aaad6d18d8e83f13ef
Antonio Tornero <antoniotornero@Antonios-MacBook-Pro.local>
2024-07-06 16:53:30 +0700

    esqueleton prompt

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,4 +1,10 @@
+import json
+import os
+
 from flask import jsonify
+
+DATA_FILE = 'data_test.json'
+DATA_USER_FILE = 'data_user.json'
 
 
 def get_all_exercises():
@@ -18,3 +24,19 @@ def get_all_exercises():
             return jsonify({"error": "Invalid response format in stored data"}), 500
 
     return all_responses
+
+
+def get_user_data():
+    if os.path.exists(DATA_USER_FILE):
+        with open(DATA_USER_FILE, 'r') as f:
+            existing_data = json.load(f)
+    else:
+        existing_data = []
+    return existing_data
+
+def generate_prompt(user_prompt):
+    prompt = "";
+    exercises = get_all_exercises()
+    user_data = get_user_data()
+
+    return prompt
